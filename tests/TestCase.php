22e5df1da082cd1fecf1bab6c6ad69f7e93bc118
Maicol <maicolbattistini@live.it>
2020-09-07 17:03:29 +0200

    test: ✅ Updated test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,29 +1,33 @@
 <?php
+namespace {
+    require_once "vendor/illuminate/support/helpers.php";
+}
 
-namespace Maicol07\Flarum\Api\Tests;
-
-use Maicol07\Flarum\Api\Client;
-use Maicol07\Flarum\Api\Models\Model;
-
-abstract class TestCase extends \PHPUnit\Framework\TestCase
-{
-    /**
-     * @var Client
-     */
-    protected $client;
+namespace Maicol07\Flarum\Api\Tests {
     
-    protected function setUp(): void
+    use Maicol07\Flarum\Api\Client;
+    use Maicol07\Flarum\Api\Models\Model;
+    
+    abstract class TestCase extends \PHPUnit\Framework\TestCase
     {
-        parent::setUp();
+        /**
+         * @var Client
+         */
+        protected $client;
         
-        $token = getenv('FLARUM_TOKEN');
-    
-        $this->client = new Client(
-            empty(getenv('FLARUM_HOST')) ? 'https://discuss.flarum.org' : getenv('FLARUM_HOST'),
-            $token ? compact('token') : [],
-            env('DEBUG') ? ['debug' => true] : []
-        );
-    
-        Model::setDispatcher($this->client);
+        protected function setUp(): void
+        {
+            parent::setUp();
+            
+            $token = env('FLARUM_TOKEN');
+            
+            $this->client = new Client(
+                env('FLARUM_HOST') ?? 'https://discuss.flarum.org',
+                $token ? compact('token') : [],
+                env('DEBUG') ? ['debug' => true] : []
+            );
+            
+            Model::setDispatcher($this->client);
+        }
     }
 }
